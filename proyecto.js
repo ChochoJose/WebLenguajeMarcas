140ebc055c8fd9429ee0336564c4bab457b359b7
Miguel <miguel.mathe13@gmail.com>
2021-03-13 21:25:24 +0100

    Proyectos

---
+++ proyecto.js
@@ -0,0 +1,15 @@
+var inicio = 0;
+carrusel();
+function carrusel(){
+	var i;
+	var x = document.getElementsByClassName("fotosGal");
+	for (var i = 0; i < x.length ; i++) {
+		x[i].style.display = "none";
+	}
+	inicio++;
+	if (inicio > x.length) {
+		inicio = 1;
+	}
+	x[inicio - 1].style.display = "block";
+	setTimeout(carrusel,3000);
+}
